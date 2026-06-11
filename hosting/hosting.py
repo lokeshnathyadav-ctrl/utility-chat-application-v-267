3ad6243535d0ab00970f412f60c6a8cbed20c3fc
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-06-11 19:08:06 +0530

    changes in hosting script

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -1,7 +1,6 @@
 from huggingface_hub import HfApi
 import os
-hf_api_key = os.getenv('HF_TOKEN')
-api = HfApi(token=HF_TOKEN)
+api = HfApi(token=os.getenv("HF_TOKEN")
 api.upload_folder(
     folder_path = "deployment",
     repo_id = "Lokeshnathy/ChatBot-v-267",
