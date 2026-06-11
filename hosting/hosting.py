97d921cb1e5a7d39503bd1a4ccedd8e56fe4051b
lokeshnathyadav-ctrl <lokeshnath.yadav@gmail.com>
2026-06-11 19:10:20 +0530

    Deploying to HF Space

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -1,6 +1,6 @@
 from huggingface_hub import HfApi
 import os
-api = HfApi(token=os.getenv("HF_TOKEN")
+api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path = "deployment",
     repo_id = "Lokeshnathy/ChatBot-v-267",
